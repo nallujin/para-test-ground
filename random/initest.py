5f9fb5a85c2c538626aa7e15d81826ffec0d35a5
Benediktus 'Puja' Wisasongko <81706750+pujawisasongko@users.noreply.github.com>
2022-12-13 21:11:45 +0700

    Add files via upload

---
+++ random/initest.py
@@ -0,0 +1,5 @@
+name = 'World'
+spaces = ''
+for miber in name:
+    print(spaces + miber)
+    spaces += ' '
---
+++ random/strings.py
@@ -0,0 +1,5 @@
+phrase = 'Girrafe Academy'
+
+print(phrase.index('Acad'))  # Outputs 8
+print(phrase[3])  # Outputs r
+print(phrase.replace('Girrafe', 'Elephant'))  # Outputs - Elephant Academy
