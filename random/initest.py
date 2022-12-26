ef474f5d1333875b7e91c16300b3b62c761f6d9c
Puja Wisasongko <81706750+pujawisasongko@users.noreply.github.com>
2022-12-26 20:01:21 +0700

    Rearranged repo's directory

--- random/initest.py
+++
@@ -1,5 +0,0 @@
-name = 'World'
-spaces = ''
-for miber in name:
-    print(spaces + miber)
-    spaces += ' '
--- random/strings.py
+++
@@ -1,5 +0,0 @@
-phrase = 'Girrafe Academy'
-
-print(phrase.index('Acad'))  # Outputs 8
-print(phrase[3])  # Outputs r
-print(phrase.replace('Girrafe', 'Elephant'))  # Outputs - Elephant Academy
