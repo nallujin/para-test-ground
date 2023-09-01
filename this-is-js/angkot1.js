696202da1675dd9cc9ec68da189fa9bba875250e
nallujin <benedictuspujagarfield@gmail.com>
2023-09-01 22:26:39 +0700

    Finished WPU JS exercise

---
+++ this-is-js/angkot1.js
@@ -0,0 +1,9 @@
+// lang-ina: file/source code ini ditujukan sebagai latihan JavaScript dasar yang diberikan oleh WPU.
+// lang-eng: this file is intended to used as an answer to the WPU's basic JavaScript exercise.
+
+
+let angkotNo = 1;
+while(angkotNo <= 10){
+    console.log(`Angkot No. ${angkotNo} beroperasi dengan baik.`);
+    angkotNo++;
+}
